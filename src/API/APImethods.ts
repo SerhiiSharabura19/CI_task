423a40134fd75ef4e772995751993e2edcf876d2
Serhii <serhiisharaburaqa@gmail.com>
2026-03-10 22:12:03 +0200

    APIenvFix

--- src/API/APImethods.ts
+++ src/API/APImethods.ts
@@ -10,69 +10,59 @@ export class APIMethods {
 
   constructor(request: APIRequestContext) {
     this.request = request;
-    this.productsListEndpoint = process.env.PRODUCTS_LIST_ENDPOINT!;
-    this.brandsListEndpoint = process.env.BRANDS_LIST_ENDPOINT!;
-    this.verifyLoginEndpoint = process.env.VERIFY_LOGIN_ENDPOINT!;
+    this.productsListEndpoint = process.env.PRODUCTS_LIST_ENDPOINT;
+    this.brandsListEndpoint = process.env.BRANDS_LIST_ENDPOINT;
+    this.verifyLoginEndpoint = process.env.VERIFY_LOGIN_ENDPOINT;
   }
 
   async getAllProducts() {
-    const response = await this.request.get(
-      this.productsListEndpoint
-    );
+    const response = await this.request.get(this.productsListEndpoint);
 
     return response;
   }
 
   async postToAllProductsList() {
-    const response = await this.request.post(
-      this.productsListEndpoint
-    );
+    const response = await this.request.post(this.productsListEndpoint);
 
     return response;
   }
 
   async putToAllBrandsList() {
-    const response = await this.request.put(
-      this.brandsListEndpoint
-    );
+    const response = await this.request.put(this.brandsListEndpoint);
 
     return response;
   }
 
   async deleteToVerifyLogin() {
-    const response = await this.request.delete(
-      this.verifyLoginEndpoint
-    );
+    const response = await this.request.delete(this.verifyLoginEndpoint);
 
     return response;
   }
 
   async assertResponseHas200Code(response: APIResponse) {
-    await test.step(`Verify that the responce has 200 code`, async() => {
+    await test.step(`Verify that the responce has 200 code`, async () => {
       expect(response.status()).toBe(200);
-    })
+    });
   }
 
   async assertResponseHas405Code(response: APIResponse) {
-    await test.step(`Verify that the responce has 405 code`, async() => {
+    await test.step(`Verify that the responce has 405 code`, async () => {
       const body = await response.json();
       expect(body.responseCode).toBe(405);
-    }); 
+    });
   }
 
   async assertResponseMessage(response: APIResponse) {
-    await test.step(`Verify that the responce has the "This request method is not supported." message`, async() => {
+    await test.step(`Verify that the responce has the "This request method is not supported." message`, async () => {
       const body = await response.json();
-      expect(body.message).toBe("This request method is not supported.");
-    }); 
+      expect(body.message).toBe('This request method is not supported.');
+    });
   }
 
   async assertResponseBodyToHaveProperty(response: APIResponse, property: string) {
-    await test.step(`Verify that the responce has "products" property`, async() => {
+    await test.step(`Verify that the responce has "products" property`, async () => {
       const body = await response.json();
       expect(body).toHaveProperty(property);
-    })
+    });
   }
-
-
 }

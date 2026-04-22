68b157c6b86fcdca46b5ac877a27a359f796906c
Serhii <serhiisharaburaqa@gmail.com>
2026-04-22 08:46:31 +0300

    Deleted Constants file

--- tests/global.teardown.ts
+++ tests/global.teardown.ts
@@ -2,11 +2,11 @@ import { test as teardown } from './_fixtures/fixtures';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { DB_PATH } from './constants';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const USER_PATH = path.resolve(__dirname, '.data/user.json');
+const DB_PATH = path.resolve(__dirname, '.data/users.db');
 
 teardown('delete user via API', async ({ apiManager }) => {
   const user = JSON.parse(fs.readFileSync(USER_PATH, 'utf-8'));
